a9ce117d35f6b79beb49209803579735e4ffda46
Hardik Choksi <hardik@mw.io>
2025-08-08 10:26:42 +0530

    Add instrumentation for APM and source map uploading

---
+++ instrumentation.ts
@@ -0,0 +1,14 @@
+// @ts-expect-error: No types available for '@middleware.io/agent-apm-nextjs'
+import tracker from '@middleware.io/agent-apm-nextjs';
+
+export function register() {
+    tracker.track({
+        serviceName: "wrkm-nextjs",
+        accessToken: process.env.MW_ACCESS_TOKEN || '',
+        enableExceptionHandling: true, // Enable advanced exception handling
+        // target: "vercel",
+        customResourceAttributes: {
+            "app.version": "0.0.1"
+        }
+    });
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+import MiddlewareWebpackPlugin from "@middleware.io/sourcemap-uploader/dist/webpack-plugin";
+
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.devtool = "source-map";
+    }
+
+    config.plugins.push(
+      new MiddlewareWebpackPlugin(
+        process.env.MW_ACCESS_TOKEN || "", // Account key of the application.
+        "0.0.1", // Application verison
+        ".next/", // By default path of next.js where sourcemap resides.
+        ".next/" // Base path where your sourcemap will reside after upload.
+      )
+    );
+    return config;
+  },
+  experimental: {
+    serverSourceMaps: true,
+    instrumentationHook: true,
+    serverComponentsExternalPackages: ["@middleware.io/agent-apm-nextjs"],
+  },
 };
 
 export default nextConfig;
