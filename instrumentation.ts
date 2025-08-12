f67c8b4e9ad8eddd3d270ed184434944ff18af4a
Hardik Choksi <hardik.choksi@middleware.io>
2025-08-12 12:54:26 +0530

    Remove APM and source map uploader dependencies and commented out code in configuration files

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,14 +1,14 @@
-// @ts-expect-error: No types available for '@middleware.io/agent-apm-nextjs'
-import tracker from '@middleware.io/agent-apm-nextjs';
+//// @ts-expect-error: No types available for '@middleware.io/agent-apm-nextjs'
+// import tracker from '@middleware.io/agent-apm-nextjs';
 
 export function register() {
-    tracker.track({
-        serviceName: "wrkm-nextjs-myapp2",
-        accessToken: process.env.MW_ACCESS_TOKEN || '',
-        enableExceptionHandling: true, // Enable advanced exception handling
-        target: "vercel",
-        customResourceAttributes: {
-            "app.version": "0.0.1"
-        }
-    });
+    // tracker.track({
+    //     serviceName: "wrkm-nextjs-myapp2",
+    //     accessToken: process.env.MW_ACCESS_TOKEN || '',
+    //     enableExceptionHandling: true, // Enable advanced exception handling
+    //     target: "vercel",
+    //     customResourceAttributes: {
+    //         "app.version": "0.0.1"
+    //     }
+    // });
 }
